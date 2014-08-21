bd5e969e1e048269731f24e9e3bf6b0ba1406b12
Hugo Doria <hugo@popcode.com.br>
2014-08-20 23:20:06 -0300

    minor fix on javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,13 @@ $(document).ready(function() {
   $('a.blog-button').click(function() {
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return;
     currentWidth = $('.panel-cover').width();
-    if (currentWidth < 960) {
+    // if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed');
       $('.content-wrapper').addClass('animated slideInRight');
-    } else {
-      $('.panel-cover').css('max-width',currentWidth);
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function() {} );
-    }
+    // } else {
+    //   $('.panel-cover').css('max-width',currentWidth);
+    //   $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function() {} );
+    // }
   });
 
   if (window.location.hash && window.location.hash == "#blog") {
